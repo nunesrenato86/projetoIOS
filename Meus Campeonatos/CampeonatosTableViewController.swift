2ae9e7a53e55113c337e67a4e726472a3e9b4eb0
iossenac <iossenac@sala309pc01.local>
2016-09-17 12:04:17 -0300

    TableView funcional com dados mock sem modelo de dados

---
+++ Meus Campeonatos/CampeonatosTableViewController.swift
@@ -0,0 +1,102 @@
+//
+//  CampeonatosTableViewController.swift
+//  Meus Campeonatos
+//
+//  Created by iossenac on 17/09/16.
+//  Copyright © 2016 Renato Nunes de Nunes. All rights reserved.
+//
+
+import UIKit
+
+class CampeonatosTableViewController: UITableViewController {
+    
+    
+    // MARK: - Model
+    let campeonatos = [
+        (ano:2016, categoria:"Open", parceiro:"Nícolas", nome:"Brasileiro", local:"POA"),
+        (ano:2016, categoria:"Segunda", parceiro:"Bauer", nome:"Master 1E", local:"POA"),
+        (ano:2016, categoria:"Segunda", parceiro:"Nícolas", nome:"Estadual", local:"POA"),
+        (ano:2016, categoria:"Segunda", parceiro:"Nícolas", nome:"Master 2E", local:"POA")
+    ]
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+
+    // MARK: - Table view data source
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return campeonatos.count
+    }
+
+    
+    //selecionar uma celula pra uma linha qualquer
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = campeonatos[indexPath.row].nome
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
